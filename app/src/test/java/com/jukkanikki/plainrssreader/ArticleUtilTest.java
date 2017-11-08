e62d67927e03aa35080b4c0be542b9c864088390
nikkijuk <voitava453auto>
2017-11-08 17:19:41 +0100

    added junit test for reading json

--- app/src/test/java/com/jukkanikki/plainrssreader/ArticleUtilTest.java
+++ app/src/test/java/com/jukkanikki/plainrssreader/ArticleUtilTest.java
@@ -7,19 +7,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Created by jnikki on 11/8/17.
+ * Test unit of work - does not have dependecies to android
  */
-
-
 public class ArticleUtilTest {
 
-    String json = "{\"status\":\"ok\",\"feed\":{\"url\":\"http://rss.nytimes.com/services/xml/rss/nyt/Science.xml\",\"title\":\"NYT &gt; Science\",\"link\":\"https://www.nytimes.com/section/science?partner=rss&amp;emc=rss\",\"author\":\"\",\"description\":\"\",\"image\":\"https://static01.nyt.com/images/misc/NYT_logo_rss_250x40.png\"},\"items\":[{\"title\":\"ScienceTake: Wolves Know How to Work Together\",\"pubDate\":\"2017-11-07 08:30:31\",\"link\":\"https://www.nytimes.com/2017/11/07/science/wolves-dogs-cooperation.html?partner=rss&amp;emc=rss\",\"guid\":\"https://www.nytimes.com/2017/11/07/science/wolves-dogs-cooperation.html\",\"author\":\"JAMES GORMAN\",\"thumbnail\":\"\",\"description\":\"Wolves far outperformed dogs on a widely used test of cooperation in animals.\",\"content\":\"Wolves far outperformed dogs on a widely used test of cooperation in animals.\",\"enclosure\":[],\"categories\":[\"Dogs\",\"Wolves\",\"Proceedings of the National Academy of Sciences\"]}]}";
+    private static final String json = "{\"status\":\"ok\",\"feed\":{\"url\":\"http://rss.nytimes.com/services/xml/rss/nyt/Science.xml\",\"title\":\"NYT &gt; Science\",\"link\":\"https://www.nytimes.com/section/science?partner=rss&amp;emc=rss\",\"author\":\"\",\"description\":\"\",\"image\":\"https://static01.nyt.com/images/misc/NYT_logo_rss_250x40.png\"},\"items\":[{\"title\":\"ScienceTake: Wolves Know How to Work Together\",\"pubDate\":\"2017-11-07 08:30:31\",\"link\":\"https://www.nytimes.com/2017/11/07/science/wolves-dogs-cooperation.html?partner=rss&amp;emc=rss\",\"guid\":\"https://www.nytimes.com/2017/11/07/science/wolves-dogs-cooperation.html\",\"author\":\"JAMES GORMAN\",\"thumbnail\":\"\",\"description\":\"Wolves far outperformed dogs on a widely used test of cooperation in animals.\",\"content\":\"Wolves far outperformed dogs on a widely used test of cooperation in animals.\",\"enclosure\":[],\"categories\":[\"Dogs\",\"Wolves\",\"Proceedings of the National Academy of Sciences\"]}]}";
 
+    private static final String title1 = "ScienceTake: Wolves Know How to Work Together";
 
-    String title1 = "ScienceTake: Wolves Know How to Work Together";
-
+    /**
+     * Mashall json to pojos
+     */
     @Test
     public void parseJsonTest () {
+
         // marshal to feed
         FeedWrapper feed = ArticlesUtil.convertToObjects(json);
 
