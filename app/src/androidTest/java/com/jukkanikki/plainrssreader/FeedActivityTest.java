81ffa90209fb5152f1a9227bae239be2d6909337
nikkijuk <voitava453auto>
2017-11-08 16:44:27 +0100

    extended very dummy espresso test - works when fragment is opend on top of Activity

--- app/src/androidTest/java/com/jukkanikki/plainrssreader/FeedActivityTest.java
+++ app/src/androidTest/java/com/jukkanikki/plainrssreader/FeedActivityTest.java
@@ -1,5 +1,6 @@
 package com.jukkanikki.plainrssreader;
 
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,9 +11,13 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+/**
+ * Test interactions starting from main FeedActivity
+ */
 @RunWith(AndroidJUnit4.class)
 public class FeedActivityTest {
 
@@ -21,8 +26,20 @@ public class FeedActivityTest {
             new ActivityTestRule<>(FeedActivity.class);
 
     @Test
-    public void ensureTextChangesWork() {
+    public void settingsButtonWorks() {
+
+        // button is visible
+        onView(withId(R.id.btnSettings)).check(matches(isDisplayed()));
+
+        // button pressed
         onView(withId(R.id.btnSettings)).perform(click());
+
+        // title of fragement is shown
+        onView(withText("PlainRssReader")).check((matches(isDisplayed())));
+
+        // url field is shown
+        onView(withText("Url")).check((matches(isDisplayed())));
+
     }
 
 
